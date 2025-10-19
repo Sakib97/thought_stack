a8a24042e52bc32b0afa8b281e69b77301464d31
Sakib97 <ashekseum86@gmail.com>
2025-10-20 01:23:21 +0600

    feat: Implement dynamic meta tags directly in ArticleDetails component for improved SEO

--- src/features/articles/pages/ArticleDetails.jsx
+++ src/features/articles/pages/ArticleDetails.jsx
@@ -9,9 +9,9 @@ import ArticleReactions from "../components/ArticleReactions";
 import { useAuth } from "../../../context/AuthProvider";
 import ArticleComment from "../components/ArticleComment";
 import Spinner from 'react-bootstrap/Spinner';
-// import { Helmet } from "@dr.pogodin/react-helmet";
-// import { Helmet } from "react-helmet-async";
-// import { Helmet } from "@dr.pogodin/react-helmet";
+// import { Helmet } from "react-helmet";
+
+
 
 
 const ArticleDetails = () => {
@@ -112,168 +112,164 @@ const ArticleDetails = () => {
 
 
     return (
-        <div>
-
-            {/* dynamic meta tags */}
-            {/* <Helmet> */}
-            <article>
-                <title>{title} | Thought Stack</title>
-                <meta name="description" content={description} />
-                <meta property="og:type" content="article" />
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
-                <meta property="og:image" content={imageUrl} />
-                <meta property="og:url" content={url} />
-                <meta property="og:site_name" content="Thought Stack" />
+        <>
+            <title>{`${title} | Thought Stack`}</title>
+            <meta name="description" content={description} />
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={imageUrl} />
+            <meta property="og:url" content={url} />
+            <meta property="og:site_name" content="Thought Stack" />
 
 
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={title} />
-                <meta name="twitter:description" content={description} />
-                <meta name="twitter:image" content={imageUrl} />
-                {/*  </Helmet> */}
-          </article>
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={imageUrl} />
 
-            <link
-                href="https://fonts.googleapis.com/css2?family=Noto+Serif+Bengali:wght@100..900&display=swap"
-                rel="stylesheet"
-            />
-            <div className={`${styles.article}`}>
-                <div className={`containers ${styles.articleContainer}`}>
-                    <div className={`${styles.articleHead}`}>
-                        <h6 style={{ color: 'grey', fontFamily: fontFamily }}>
-                            {language === "en" ? article.event_title_en
-                                : article.event_title_bn} </h6>
+            <div>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Serif+Bengali:wght@100..900&display=swap"
+                    rel="stylesheet"
+                />
+                <div className={`${styles.article}`}>
+                    <div className={`containers ${styles.articleContainer}`}>
+                        <div className={`${styles.articleHead}`}>
+                            <h6 style={{ color: 'grey', fontFamily: fontFamily }}>
+                                {language === "en" ? article.event_title_en
+                                    : article.event_title_bn} </h6>
 
-                        <h1 style={{ fontFamily: fontFamily }}>
-                            {language === "en"
-                                ? article.title_en
-                                : article.title_bn}
-                        </h1>
-                        <span style={{ fontSize: '19px', fontFamily: fontFamily }}>
-                            {language === "en"
-                                ? article.subtitle_en
-                                : article.subtitle_bn}
-                        </span>
-                        <img
-                            src={article.cover_img_link}
-                            alt={
-                                language === "en"
+                            <h1 style={{ fontFamily: fontFamily }}>
+                                {language === "en"
                                     ? article.title_en
-                                    : article.title_bn
-                            }
-                            className={styles.articleImage}
-                        />
+                                    : article.title_bn}
+                            </h1>
+                            <span style={{ fontSize: '19px', fontFamily: fontFamily }}>
+                                {language === "en"
+                                    ? article.subtitle_en
+                                    : article.subtitle_bn}
+                            </span>
+                            <img
+                                src={article.cover_img_link}
+                                alt={
+                                    language === "en"
+                                        ? article.title_en
+                                        : article.title_bn
+                                }
+                                className={styles.articleImage}
+                            />
 
-                        <div
-                            className={`${styles.articleImageCaption}`}
-                            style={{ fontFamily: fontFamily }}
-                        >
-                            {language === "en"
-                                ? article.cover_img_cap_en
-                                : article.cover_img_cap_bn}
-                        </div>
-
-                        <div className={`${styles.authorAndDateOfArticle}`}>
-                            <div className={`${styles.authorPicOfArticle}`}>
-                                <img
-                                    src={article.author_img_link}
-                                    alt={article.author_name}
-                                    className={styles.authorPicOfArticle}
-                                />
+                            <div
+                                className={`${styles.articleImageCaption}`}
+                                style={{ fontFamily: fontFamily }}
+                            >
+                                {language === "en"
+                                    ? article.cover_img_cap_en
+                                    : article.cover_img_cap_bn}
                             </div>
-                            <div className={`${styles.authorNameOfArticle}`}>
-                                <span
-                                    style={{
-                                        color: "black",
-                                        fontSize: "18px",
-                                        fontWeight: "bold",
-                                        fontFamily: fontFamily,
-                                    }}
-                                >
-                                    {article.author_name}
-                                </span>
 
-                                <br />
-                                <div
-                                    style={{ fontSize: "13px", color: "gray" }}
-                                >
-                                    {getFormattedTime(article.created_at)}
+                            <div className={`${styles.authorAndDateOfArticle}`}>
+                                <div className={`${styles.authorPicOfArticle}`}>
+                                    <img
+                                        src={article.author_img_link}
+                                        alt={article.author_name}
+                                        className={styles.authorPicOfArticle}
+                                    />
+                                </div>
+                                <div className={`${styles.authorNameOfArticle}`}>
+                                    <span
+                                        style={{
+                                            color: "black",
+                                            fontSize: "18px",
+                                            fontWeight: "bold",
+                                            fontFamily: fontFamily,
+                                        }}
+                                    >
+                                        {article.author_name}
+                                    </span>
+
+                                    <br />
+                                    <div
+                                        style={{ fontSize: "13px", color: "gray" }}
+                                    >
+                                        {getFormattedTime(article.created_at)}
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <hr />
+                        <hr />
 
-                    {/* Article Body */}
-                    <div className={`${styles.articleBody}`}>
-                        <div
-                            style={{ textAlign: "justify", fontSize: "18px" }}
-                            className={`${styles.articleBodyText}`}
-                        >
+                        {/* Article Body */}
+                        <div className={`${styles.articleBody}`}>
                             <div
-                                style={{ textAlign: "justify" }}
-                                className={styles.articleBodyText}
+                                style={{ textAlign: "justify", fontSize: "18px" }}
+                                className={`${styles.articleBodyText}`}
                             >
                                 <div
+                                    style={{ textAlign: "justify" }}
+                                    className={styles.articleBodyText}
+                                >
+                                    <div
+                                        style={{
+                                            textAlign: "justify",
+                                            fontFamily: fontFamily,
+                                        }}
+                                        className={styles.articleBodyText}
+                                        dangerouslySetInnerHTML={{
+                                            __html:
+                                                language === "en"
+                                                    ? article.content_en
+                                                    : article.content_bn,
+                                        }}
+                                    />
+                                </div>
+                            </div>
+
+                            <hr />
+                            {/* disclaimer section*/}
+                            <div>
+                                <div
                                     style={{
-                                        textAlign: "justify",
+                                        fontStyle: "italic",
+                                        fontSize: "14px",
+                                        color: "#666",
+                                        marginBottom: "13px",
                                         fontFamily: fontFamily,
                                     }}
-                                    className={styles.articleBodyText}
-                                    dangerouslySetInnerHTML={{
-                                        __html:
-                                            language === "en"
-                                                ? article.content_en
-                                                : article.content_bn,
+                                >
+                                    {language === "en"
+                                        ? "* The views and opinions expressed in this article are author's own and do not necessarily reflect the publisher's point of view."
+                                        : "* লেখকের নিজস্ব মতামত "}
+                                </div>
+                                <div
+                                    style={{
+                                        fontSize: "14px",
+                                        fontWeight: "bold",
+                                        color: "#666",
+                                        marginBottom: "13px",
                                     }}
-                                />
+                                >
+                                    {article.publish_author_email && (
+                                        <>Author Email: {article.author_email}</>
+                                    )}
+                                </div>
                             </div>
                         </div>
-
                         <hr />
-                        {/* disclaimer section*/}
-                        <div>
-                            <div
-                                style={{
-                                    fontStyle: "italic",
-                                    fontSize: "14px",
-                                    color: "#666",
-                                    marginBottom: "13px",
-                                    fontFamily: fontFamily,
-                                }}
-                            >
-                                {language === "en"
-                                    ? "* The views and opinions expressed in this article are author's own and do not necessarily reflect the publisher's point of view."
-                                    : "* লেখকের নিজস্ব মতামত "}
-                            </div>
-                            <div
-                                style={{
-                                    fontSize: "14px",
-                                    fontWeight: "bold",
-                                    color: "#666",
-                                    marginBottom: "13px",
-                                }}
-                            >
-                                {article.publish_author_email && (
-                                    <>Author Email: {article.author_email}</>
-                                )}
-                            </div>
-                        </div>
+                        <ArticleReactions
+                            articleId={articleId}
+                            userId={user?.id}
+                            isActive={userMeta?.is_active}
+                        />
+                        <hr />
+                        <ArticleComment articleId={articleId} userMeta={userMeta} />
+
                     </div>
-                    <hr />
-                    <ArticleReactions
-                        articleId={articleId}
-                        userId={user?.id}
-                        isActive={userMeta?.is_active}
-                    />
-                    <hr />
-                    <ArticleComment articleId={articleId} userMeta={userMeta} />
 
                 </div>
-
             </div>
-        </div>
+        </>
     );
 };
 
